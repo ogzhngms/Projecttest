2dfcf5237c980afd44c2a5ad7fb241de9ecd7c21
ogzhngms <71395893+ogzhngms@users.noreply.github.com>
2024-01-24 17:25:06 +0300

    Update Calculator.java
    Extraction add

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
-interface Math {
-    int calculate(int x, int y);
-}
+interface Math { int calculate(int x, int y);}
 
 class Add implements Math {
     @Override
@@ -11,14 +9,19 @@ class Add implements Math {
     }
 }
 
-class Multip implements Math {
+class Extraction implements Math {
     @Override
     public int calculate(int x, int y) {
-        return x * y;
+        return x - y;
     }
 }
 
-class Div implements Math {
+class Multiply implements Math {
+    @Override
+    public int calculate(int x, int y) {return x * y;}
+}
+
+class Divide implements Math {
     @Override
     public int calculate(int x, int y) {
         if (x == 0) {
@@ -27,31 +30,31 @@ class Div implements Math {
         } else if (y == 0) {
             System.out.println("Bolme islemi icin ikinci sayi 0 olamaz.");
             return 0;
-        } else {
-            return x / y;
-        }
+        } else {return x / y;}
     }
 }
-
 public class Calculator {
     public static void main(String[] args) {
-        Scanner kullanici = new Scanner(System.in);
+        Scanner user = new Scanner(System.in);
 
         while (true) {
-            System.out.print("Toplam/cikarma \"1\" \nCarpma \"2\" \nbolme \"3\"\n\nLutfen Secim Yapiniz: ");
-            char mathchoise = kullanici.next().charAt(0);
+            System.out.print("Toplam\"1\"\nCikarma \"2\" \nCarpma \"3\" \nbolme \"4\"\n\nLutfen Secim Yapiniz: ");
+            char mathChoise = user.next().charAt(0);
 
             Math object;
 
-            switch (mathchoise) {
+            switch (mathChoise) {
                 case '1':
                     object = new Add();
                     break;
                 case '2':
-                    object = new Multip();
+                    object = new Extraction();
                     break;
                 case '3':
-                    object = new Div();
+                    object = new Divide();
+                    break;
+                case '4':
+                    object = new Multiply();
                     break;
                 default:
                     System.out.println("Gecersiz Islem. Lutfen tekrar deneyiniz");
@@ -59,20 +62,17 @@ public class Calculator {
             }
 
             System.out.print("Sayi giriniz: ");
-            int x = kullanici.nextInt();
+            int x = user.nextInt();
             System.out.print("Sayi giriniz: ");
-            int y = kullanici.nextInt();
+            int y = user.nextInt();
 
             int result = object.calculate(x, y);
-
             System.out.println("\nSonuc = " + result);
 
             System.out.print("\nDevam etmek istiyor musunuz?(E/H): ");
-            String devam = kullanici.next();
-            if (!devam.equalsIgnoreCase("e")) {
-                break;
-            }
+            String continuee = user.next();
+            if (!continuee.equalsIgnoreCase("e")) {break;}
         }
-        kullanici.close();
+        user.close();
     }
 }
